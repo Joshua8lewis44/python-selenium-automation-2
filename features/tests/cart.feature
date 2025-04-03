004f6d7efd45c76be90d6f9387a0e2b114aa9436
joshua8lewis44 <joshua8lewis44@gmail.com>
2025-04-03 11:44:08 -0700

    update search script

---
+++ features/tests/cart.feature
@@ -0,0 +1,6 @@
+# Created by joshzz at 4/3/25
+Feature: # Enter feature name here
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    # Enter steps here
--- sample_script.py
+++ sample_script.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 #get the path from webdriver executable
@@ -10,7 +12,9 @@ driver_path = ChromeDriverManager().install()
 #create a new chrome browser instance
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
+#driver.implicitly_wait(8)
 driver.maximize_window()
+driver.wait = WebDriverWait(driver, 10)
 
 # open the url
 driver.get('https://www.google.com/')
@@ -20,10 +24,13 @@ search.clear()
 search.send_keys('car')
 
 # wait for 4 sec
-sleep(4)
+#sleep(10)
+driver.wait.until(EC.element_to_be_clickable(By.NAME, 'btnK'))
 
 # click search
-driver.find_element(By.NAME, 'btnK').click()
+#driver.find_element(By.NAME, 'btnK').click()
+
+sleep(3)
 
 # verify
 assert 'car' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
